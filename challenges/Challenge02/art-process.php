d21e27da4887c1ad74d95a823d05060e927f9616
GoncaloNevesCorreia <57664115+GoncaloNevesCorreia@users.noreply.github.com>
2021-01-20 17:13:02 +0000

    Challenges acabados

--- challenges/Challenge02/art-process.php
+++ challenges/Challenge02/art-process.php
@@ -1,5 +1,62 @@
 <?php 
 
+// WITH ERROR VALIDATION
+$namesToValidate = ["title", "description", "genre", "subject", "medium", "year", "museum"];
+$errorTypes = ["errorTitle", "errorDescription", "errorGenre", "errorSubject", "errorMedium", "errorYear", "errorMuseum"];
+
+$errors = checkForErrors($namesToValidate, $errorTypes);
+if (strlen($errors) > 0) {
+  header('Location: challenge02.php' . $errors);
+  exit;
+} 
+
+function checkForErrors($pNames, $pErrors){
+  $errorString = "?";
+
+  for ($i=0; $i < count($pNames); $i++) { 
+    $name = $pNames[$i];
+    if (!isValidPOST($name)) {
+      $errorString .= $pErrors[$i] . '&';
+    }
+  }
+
+  return substr($errorString, 0, -1); 
+}
+
+function isValidPOST($name) {
+  return (isset($_POST[$name]) && $_POST[$name] !== "");
+}
+// WITH ERROR VALIDATION
+
+
+
+// WITH NO ERROR VALIDATION
+
+//$namesToValidate = ["title", "description", "genre", "subject", "medium", "year", "museum"];
+// if (!validate_POSTS($namesToValidate)) {
+//     header('Location: challenge02.php');
+//     exit;
+// }
+
+// function validate_POSTS($names){
+//   foreach ($names as $name) {
+//     if ( !(isset($_POST[$name]) && $_POST[$name] !== "") ) return false;
+//   }
+//   return true;
+// }
+// WITH NO ERROR VALIDATION
+  
+  function clean_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+  }
+
+  function print_Value_From_POST($name){
+    if (!isset($name)) return;
+    echo clean_input($_POST[$name]);
+  }
 ?>
 <!DOCTYPE html>
 <html>
@@ -22,31 +79,31 @@
       <caption class="results__caption">Art Work Saved</caption>
       <tr>
         <td class="results__label">Title</td>    
-        <td class="results__value"></td> 
+        <td class="results__value"><?php print_Value_From_POST("title"); ?></td> 
       </tr>
       <tr>
         <td class="results__label">Description</td>    
-        <td class="results__value"></td> 
+        <td class="results__value"><?php print_Value_From_POST("description"); ?></td> 
       </tr>
       <tr>
         <td class="results__label">Genre</td>    
-        <td class="results__value"></td> 
+        <td class="results__value"><?php print_Value_From_POST("genre"); ?></td> 
       </tr>
       <tr>
         <td class="results__label">Subject</td>    
-        <td class="results__value"></td> 
+        <td class="results__value"><?php print_Value_From_POST("subject"); ?></td> 
       </tr>
       <tr>
         <td class="results__label">Medium</td>    
-        <td class="results__value"></td> 
+        <td class="results__value"><?php print_Value_From_POST("medium"); ?></td> 
       </tr>   
       <tr>
         <td class="results__label">Year</td>    
-        <td class="results__value"></td> 
+        <td class="results__value"><?php print_Value_From_POST("year"); ?></td> 
       </tr>  
       <tr>
         <td class="results__label">Museum</td>    
-        <td class="results__value"></td> 
+        <td class="results__value"><?php print_Value_From_POST("museum"); ?></td> 
       </tr>          
     </table>
     
